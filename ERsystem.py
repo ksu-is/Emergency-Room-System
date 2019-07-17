35e6db26438910d870125bf8bf447c26048f4adc
Lasfaw <51351975+Lasfaw@users.noreply.github.com>
2019-07-17 10:02:35 -0400

    Create ERsystem.py

---
+++ ERsystem.py
@@ -0,0 +1,7 @@
+clearance_level = input("Please enter your clearance level here: ")
+if clearance_level==1:
+    print("You may add or search for a patient record")
+elif clearance_level==2:
+    print("You may calculate the average waiting time for a patient")
+else:
+    pass
